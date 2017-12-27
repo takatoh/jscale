9bb4f43a4e87c2b3d4c6c18cf444d386f9a75ab3
takatoh <takatoh.m@gmail.com>
2017-12-27 16:11:43 +0900

    main.go: Implement conpose and sort waves.

--- main.go
+++ main.go
@@ -3,21 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
+	"math"
+	"sort"
 
 	"github.com/takatoh/jscale/wave"
 )
 
 func main() {
+	var x, y, z []complex128
+	var c []float64
+
 	csvfile := os.Args[1]
 
 	waves := wave.LoadCSV(csvfile)
-	x := waves[0]
-	y := waves[1]
-	z := waves[2]
+//	dt := waves[0].Dt
+	n := len(waves[0].Data)
+	nn := int(math.Pow(2.0, math.Ceil(math.Log10(float64(n)) / math.Log10(2.0))))
 
-	n := len(x.Data)
-	fmt.Printf("%s,%s,%s\n", x.Name, y.Name, z.Name)
 	for i := 0; i < n; i++ {
-		fmt.Printf("%f,%f,%f\n", x.Data[i], y.Data[i], z.Data[i])
+		x = append(x, complex(waves[0].Data[i], 0.0))
+		y = append(y, complex(waves[1].Data[i], 0.0))
+		z = append(z, complex(waves[2].Data[i], 0.0))
+	}
+	for i := n; i < nn; i++ {
+		x = append(x, complex(0.0, 0.0))
+		y = append(y, complex(0.0, 0.0))
+		z = append(z, complex(0.0, 0.0))
+	}
+
+	for i := 0; i < nn; i++ {
+		xr := real(x[i])
+		yr := real(y[i])
+		zr := real(z[i])
+		c = append(c, math.Sqrt(xr * xr + yr * yr + zr * zr))
+	}
+	sort.Slice(c, func(i, j int) bool {
+		return c[i] > c[j]
+	})
+
+	for i := 0; i < nn; i++ {
+		fmt.Println(c[i])
 	}
 }
