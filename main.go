1497d4467a7c37dba15209a94bed0369bb97ce76
takatoh <takatoh.m@gmail.com>
2018-07-07 11:31:46 +0900

    main.go(calcIntensity): New and use.

--- main.go
+++ main.go
@@ -36,9 +36,6 @@ Options:
 		os.Exit(0)
 	}
 
-	var x, y, z []complex128
-	var c []float64
-
 	csvfile := flag.Args()[0]
 
 	var waves []*wave.Wave
@@ -57,14 +54,46 @@ Options:
 		}
 	}
 
-	dt := waves[0].Dt
-	n := len(waves[0].Data)
-	nn := int(math.Pow(2.0, math.Ceil(math.Log10(float64(n)) / math.Log10(2.0))))
+	I := calcIntensity(waves[0], waves[1], waves[2])
+
+	fmt.Printf("計測震度 %.1f\n", I)
+	if I < 0.5 {
+		fmt.Println("震度0")
+	} else if I < 1.5 {
+		fmt.Println("震度1")
+	} else if I < 2.5 {
+		fmt.Println("震度2")
+	} else if I < 3.5 {
+		fmt.Println("震度3")
+	} else if I < 4.5 {
+		fmt.Println("震度4")
+	} else if I < 5.0 {
+		fmt.Println("震度5弱")
+	} else if I < 5.5 {
+		fmt.Println("震度5強")
+	} else if I < 6.0 {
+		fmt.Println("震度6弱")
+	} else if I < 6.5 {
+		fmt.Println("震度6強")
+	} else {
+		fmt.Println("震度7")
+	}
+}
+
+func calcIntensity(ns, ew, ud *wave.Wave) float64 {
+	var x, y, z []complex128
+	var c []float64
+	var dt float64
+	var n, nn int
+
+	dt = ns.Dt
+	n = len(ns.Data)
+	nn = int(math.Pow(2.0, math.Ceil(math.Log10(float64(n)) / math.Log10(2.0))))
 
 	for i := 0; i < n; i++ {
-		x = append(x, complex(waves[0].Data[i], 0.0))
-		y = append(y, complex(waves[1].Data[i], 0.0))
-		z = append(z, complex(waves[2].Data[i], 0.0))
+		x = append(x, complex(ns.Data[i], 0.0))
+		y = append(y, complex(ew.Data[i], 0.0))
+		z = append(z, complex(ud.Data[i], 0.0))
 	}
 	for i := n; i < nn; i++ {
 		x = append(x, complex(0.0, 0.0))
@@ -98,26 +127,5 @@ Options:
 	I := 2 * math.Log10(a) + 0.94
 	I = math.Floor(math.Floor(I * 100.0 + 0.5) / 10.0) / 10.0
 
-	fmt.Printf("計測震度 %.1f\n", I)
-	if I < 0.5 {
-		fmt.Println("震度0")
-	} else if I < 1.5 {
-		fmt.Println("震度1")
-	} else if I < 2.5 {
-		fmt.Println("震度2")
-	} else if I < 3.5 {
-		fmt.Println("震度3")
-	} else if I < 4.5 {
-		fmt.Println("震度4")
-	} else if I < 5.0 {
-		fmt.Println("震度5弱")
-	} else if I < 5.5 {
-		fmt.Println("震度5強")
-	} else if I < 6.0 {
-		fmt.Println("震度6弱")
-	} else if I < 6.5 {
-		fmt.Println("震度6強")
-	} else {
-		fmt.Println("震度7")
-	}
+	return I	
 }
