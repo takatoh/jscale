99d1f0e1b88a0ce50dcb401270a441a65d569f58
takatoh <takatoh.m@gmail.com>
2024-04-05 05:30:06 +0900

    lpgm/lpgm.go(HPF): New.

--- lpgm/lpgm.go
+++ lpgm/lpgm.go
@@ -44,6 +44,25 @@ func Calc(ns, ew *seismicwave.Wave) []float64 {
 	return sva
 }
 
+func HPF(acc []float64) []float64 {
+	a1 := -2.0
+	a2 := 1.0
+	b1 := -1.995438545842
+	b2 := 0.995448925627
+	g0 := 0.997721867867
+
+	accFilterd := make([]float64, len(acc))
+	accFilterd[0], accFilterd[1] = acc[0], acc[1]
+	y := make([]float64, len(acc))
+	y[0], y[1] = 0.0, 0.0
+	for t := 2; t < len(acc); t++ {
+		y[t] = acc[t] + a1*acc[t-1] + a2*acc[t-1] - b1*y[t-1] - b2*y[t-2]
+		accFilterd[t] = g0 * y[t]
+	}
+
+	return accFilterd
+}
+
 func integrate(ddy []float64, dt float64) []float64 {
 	dy, _ := integral.Iacc(ddy, dt)
 	return dy
