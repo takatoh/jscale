62365f4a33da6c08b0839713284315abfa562c8e
takatoh <takatoh.m@gmail.com>
2024-04-04 20:49:06 +0900

    lpgm/lpgm.go: Calculate Sv for period from 1.6s to 7.8s.

--- lpgm/lpgm.go
+++ lpgm/lpgm.go
@@ -7,7 +7,7 @@ import (
 	"github.com/takatoh/seismicwave"
 )
 
-func Calc(ns, ew *seismicwave.Wave) float64 {
+func Calc(ns, ew *seismicwave.Wave) []float64 {
 	n := ns.NData()
 	accNs := ns.Data
 	accEw := ew.Data
@@ -16,11 +16,19 @@ func Calc(ns, ew *seismicwave.Wave) float64 {
 		ddy[i] = math.Sqrt(accNs[i]*accNs[i] + accEw[i]*accEw[i])
 	}
 
-	t := 1.6
-	w := 2.0 * math.Pi / t
-	_, dx, _ := directintegration.Nigam(0.05, w, ns.DT(), n, ddy)
-	vel := seismicwave.Make("vel", ns.DT(), dx)
-	sv := vel.AbsMax()
+	var periods []float64
+	for t := 16; t <= 78; t += 2 {
+		periods = append(periods, float64(t)/10.0)
+	}
+	ts := len(periods)
+	sv := make([]float64, ts)
+	for i := 0; i < ts; i++ {
+		t := 1.6
+		w := 2.0 * math.Pi / t
+		_, dx, _ := directintegration.Nigam(0.05, w, ns.DT(), n, ddy)
+		vel := seismicwave.Make("vel", ns.DT(), dx)
+		sv[i] = vel.AbsMax()
+	}
 
 	return sv
 }
